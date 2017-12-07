29209a1ae4e84eac808597e0d7ea5a8245cf2476
Carmine Molisso <carmine.molisso@gmail.com>
2017-12-07 17:43:22 +0100

    Aggiunta print

--- prova/ViewController.swift
+++ prova/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("ciao")
+        print("ciao1")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
